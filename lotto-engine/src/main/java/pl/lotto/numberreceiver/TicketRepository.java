8572ecb5caf49dcce9e03f74dfa0015bf84545a5
Karol <karolkrawczyk8650@gmail.com>
2023-02-09 21:40:31 +0100

    small refactor

--- lotto-engine/src/main/java/pl/lotto/numberreceiver/TicketRepository.java
+++ lotto-engine/src/main/java/pl/lotto/numberreceiver/TicketRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public
-interface TicketRepository extends MongoRepository<LotteryTicket, String> {
+public interface TicketRepository extends MongoRepository<LotteryTicket, String> {
 }
